71cf7a14012772c94342eb5ba74cc456808f6ac5
Sergey Perepelkin <perepelkin.serge@gmail.com>
2021-02-07 03:37:34 +0100

    init inspiration site

---
+++ inspiration/js/script.js
@@ -0,0 +1,5 @@
+document.body.setAttribute("style", "background-color: #696969");
+document.querySelector(".page").style.backgroundColor = "ghostwhite";
+document.querySelector(".name").innerHTML = "DJ Rashad";
+document.querySelector("img").src = "https://www.chicagotribune.com/resizer/rerU3hB9-ZUPBsDz4VnFJWLiGLA=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/VCPD6CUIYFDKBIG5ZAUYT4BE6E.jpg";
+document.querySelector(".short-bio").className += " animated";
