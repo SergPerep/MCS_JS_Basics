ea9195af4c50e692359016763aa81f54dea83984
Sergey Perepelkin <perepelkin.serge@gmail.com>
2021-02-16 10:23:36 +0100

    added comments

--- multiples_of_six/js/script.js
+++ multiples_of_six/js/script.js
@@ -2,9 +2,13 @@ const main = document.querySelector(".main");
 
 for (let i = 0; i <= 10000; i++) {
 	if (i % 6 == 0){
+
+		// Создаем HTML объект с классом и вставляем в него число
 		const displayItem = document.createElement("div");
 		displayItem.className = "display-item";
 		displayItem.innerHTML = i;
+		
+		// Добавляем элемент на страницу
 		main.append(displayItem);
 	}
 }
